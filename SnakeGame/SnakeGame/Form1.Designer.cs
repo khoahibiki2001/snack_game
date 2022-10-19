d12d0a75a0ad0f58d0d8ff2fd3f56890c52a8579
Trang Anh Khoa <takhoaa20073@cusc.ctu.edu.vn>
2022-10-19 19:47:49 +0700

    helo

---
+++ SnakeGame/SnakeGame/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace SnakeGame
+{
+    partial class frmSnake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSnake));
+            this.picGameBoard = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picGameBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picGameBoard
+            // 
+            this.picGameBoard.Location = new System.Drawing.Point(169, 65);
+            this.picGameBoard.Name = "picGameBoard";
+            this.picGameBoard.Size = new System.Drawing.Size(550, 550);
+            this.picGameBoard.TabIndex = 0;
+            this.picGameBoard.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 500;
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "bonus1.jpg");
+            this.imgList.Images.SetKeyName(1, "bonus2.png");
+            this.imgList.Images.SetKeyName(2, "bonus3.png");
+            this.imgList.Images.SetKeyName(3, "bunus4.jpg");
+            this.imgList.Images.SetKeyName(4, "body.jpeg");
+            this.imgList.Images.SetKeyName(5, "snake_head3.png");
+            this.imgList.Images.SetKeyName(6, "wall.png");
+            // 
+            // frmSnake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 670);
+            this.Controls.Add(this.picGameBoard);
+            this.Name = "frmSnake";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picGameBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picGameBoard;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ImageList imgList;
+    }
+}
+
---
+++ SnakeGame/SnakeGame/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class frmSnake : Form
+    {
+        Random rand;
+        enum GameBoardFields
+        {
+            Free,
+            Snake,
+            Bonus
+
+        };
+        enum Directions
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        };
+
+        struct SnakeCoordinates
+        {
+            public int x;
+            public int y;
+        }
+
+        GameBoardFields[,] gameBoardFields;
+        SnakeCoordinates[] snakeXY;
+        int snakeLength;
+        Directions direction;
+        Graphics g;
+
+
+        public frmSnake()
+        {
+            InitializeComponent();
+            gameBoardFields = new GameBoardFields[11, 11];
+            snakeXY = new SnakeCoordinates[100];
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picGameBoard.Image = new Bitmap(420, 420);
+            g = Graphics.FromImage(picGameBoard.Image);
+            g.Clear(Color.White);
+
+            for(int i =1; i <= 10; i++)
+            {
+                //top and bottom walls
+                g.DrawImage(imgList.Images[6], i * 35, 0);
+                g.DrawImage(imgList.Images[6], i * 35, 385);
+                
+            }
+            for (int i = 0; i <= 11; i++)
+            {
+                //left and right walls
+                g.DrawImage(imgList.Images[6], 0, i * 35);
+                g.DrawImage(imgList.Images[6], 385, i * 35);
+
+            }
+            // initial snake body and head
+            snakeXY[0].x = 5;//head
+            snakeXY[0].y = 5;
+
+            snakeXY[1].x = 5;//first body part
+            snakeXY[1].y = 6;
+
+            snakeXY[2].x = 5;//second body part
+            snakeXY[2].x = 7;
+            g.DrawImage(imgList.Images[5], 5 * 35, 5 * 35);//head
+            g.DrawImage(imgList.Images[4], 5 * 35, 6 * 35);// first body part
+            g.DrawImage(imgList.Images[4], 5 * 35, 7 * 35);// second body part
+
+            gameBoardFields[5, 5] = GameBoardFields.Snake;
+            gameBoardFields[5, 6] = GameBoardFields.Snake;
+            gameBoardFields[5, 7] = GameBoardFields.Snake;
+
+            direction = Directions.Up;
+            snakeLength = 3;
+
+            for(int i = 0; i < 4; i++)
+            {
+                Bonus();
+            }
+
+        }
+        private void Bonus()
+        {
+     
+            int x, y;
+            var imgIndex = rand.Next(0,4);
+            do
+            {
+                x = rand.Next(1, 10);
+                y = rand.Next(1, 10);
+
+            }
+            while (gameBoardFields[x,y] != GameBoardFields.Free);
+
+            gameBoardFields[x, y] = GameBoardFields.Bonus;
+            g.DrawImage(imgList.Images[imgIndex], x * 35, y * 35);
+
+        }
+    }
+}
